703b90ddd3e65fb4d4c6fdff78d7811e20b7fd8d
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-01-18 17:10:37 +0600

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { Route, Routes } from "react-router"
+import Home from "./page/Home"
+import UserRegister from "./auth/UserRegister"
+import UserLogin from "./auth/UserLogin"
+import CaptainLogin from "./auth/CaptainLogin"
+import CaptainRegister from "./auth/CaptainRegister"
 
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="">
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/user-register" element={<UserRegister />} />
+      <Route path="/user-login" element={<UserLogin />} />
+      <Route path="/captain-login" element={<CaptainLogin />} />
+      <Route path="/captain-register" element={<CaptainRegister />} />
+    </Routes>
+    </div>
   )
 }
 
