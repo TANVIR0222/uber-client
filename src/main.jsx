8fdf9dd794ca4b702abf5ac4ef59b47ab5581e8a
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-01-18 18:51:09 +0600

    add resux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from "react-router";
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
