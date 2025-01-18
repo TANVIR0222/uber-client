65369ce20d3d6221ed27bf028d6672fde80ff3e2
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-01-18 20:31:41 +0600

    add user Api

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 // Or from '@reduxjs/toolkit/query/react'
 import { setupListeners } from '@reduxjs/toolkit/query'
+import { userApi } from './features/userApi/userApi'
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
-    // [userApi.reducerPath]: userApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(),
+    getDefaultMiddleware().concat(userApi.middleware),
 })
 
 setupListeners(store.dispatch)
